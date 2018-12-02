db493e0bd061813c3c7cac7f06d13c03b8bcb214
cangshudada <15342240569@163.com>
2018-12-02 17:10:45 +0800

    修改部分取消按钮以及回退bug

--- map.js
+++ map.js
@@ -1,14 +1,17 @@
-$(function () {
+$(function() {
     let container = document.getElementById("container"),
         map,
         flag = 0,
         click = 1,
+        k = 1,
+        p = 1,
         startPID = "",
         endPID = "",
-        startPlace="",
-        endPlace="";
+        startPlace = "",
+        endPlace = "";
 
-    GIM.REMOTE_SERVER = "https://ifs.cloudindoormap.com/webAPI/ClientService.asmx/Command?callback&";
+    GIM.REMOTE_SERVER =
+        "https://ifs.cloudindoormap.com/webAPI/ClientService.asmx/Command?callback&";
 
     // 地图初始化函数
     function initMap() {
@@ -24,12 +27,12 @@ $(function () {
             crossBuild: false, //是否跨建筑
             labelScale: 1.5, //文本缩放值
             initFloorID: "EB72B381-B3BC-40A4-B8DF-3D932302B9D8", //初始化显示的楼层 可选
-            projectID: "261FDAD7-33F9-40A4-BD33-25F1C884F54C",//csifs
+            projectID: "261FDAD7-33F9-40A4-BD33-25F1C884F54C", //csifs
             cloudID: "EFDBF244-D179-43F6-9522-A430CCFAD9D3", //云ID
             preCreation: false, //是否后台静默渲染 默认为true
             logoFiled: "Logo", //调用接口哪个字段的图片
             opacity: 1, //块的透明度
-            accessToken: "BAF3CB32-E8F2-40A6-AE1A-A5BFE2CB6E14",//腾讯分层测试
+            accessToken: "BAF3CB32-E8F2-40A6-AE1A-A5BFE2CB6E14" //腾讯分层测试
         };
 
         map = new GIM.Map3D(params); // 初始化地图
@@ -38,8 +41,6 @@ $(function () {
 
     // 初始化地图
     initMap();
-
-
 
     // 设置地图大小
     map.setSize(window.innerWidth, window.innerHeight);
@@ -53,15 +54,16 @@ $(function () {
 
     // 地图初始化之后的回调函数
     function initCompleted(data) {
-
         // 遍历动态渲染li
         data.forEach(element => {
             $(".floorContainer .floorToggleUl").append(
-                `<li class="btn" data="${element.floorID}" value="${element.Name}" id="${element.Name}">${element.Name}</li>`
+                `<li class="btn" data="${element.floorID}" value="${
+                    element.Name
+                }" id="${element.Name}">${element.Name}</li>`
             );
         });
 
-        console.log(data)
+        console.log(data);
         // 默认第一个加btn_active类
         $(".btn")
             .eq(0)
@@ -69,29 +71,18 @@ $(function () {
 
         // btn_show点击事件
 
-        $(".btn_show").on("click", function () {
-
-        })
+        $(".btn_show").on("click", function() {});
 
         // btn点击事件
-        $(".floorToggleUl .btn").on("click", function () {
-
+        $(".floorToggleUl .btn").on("click", function() {
             // 点击实现切换折叠按钮
             if (flag == 0) {
-                $(".floorContainer").removeClass(
-                    "floorToggleUl_close"
-                );
-                $(".floorContainer").addClass(
-                    "floorToggleUl_open"
-                );
+                $(".floorContainer").removeClass("floorToggleUl_close");
+                $(".floorContainer").addClass("floorToggleUl_open");
                 flag = 1;
             } else {
-                $(".floorContainer").removeClass(
-                    "floorToggleUl_open"
-                );
-                $(".floorContainer").addClass(
-                    "floorToggleUl_close"
-                );
+                $(".floorContainer").removeClass("floorToggleUl_open");
+                $(".floorContainer").addClass("floorToggleUl_close");
                 let btnNum = $(this).index(),
                     floorid = [],
                     floorName;
@@ -113,84 +104,130 @@ $(function () {
                 $(".btn_show").html(floorName);
                 flag = 0;
             }
-
-
-        }
-        );
+        });
     }
 
-    // 当前区块点击触发事件回调函数 
+    // 当前区块点击触发事件回调函数
     function onSelected(e) {
         console.log(e);
 
         // 自动寻路模块
         if (click == 1) {
+
+            // 每次点击之前初始化
+            $(".startPlace").html("");
+            $(".endPlace").html("");
+            map.clearPath(true);
+
             // 显示起点终点设置区块
             $(".setStart").show(500);
 
             // 设置起点函数
-            setStart = function () {
+            setStart = function() {
                 // 设置起点nodeId
-                map.startPID = e.nodeId;
                 startPID = e.nodeId;
+                map.startPID = e.nodeId;
+                console.log("startPID :", startPID);
                 $(".setStart").hide(500);
 
                 // 起点终点显示部分
                 $(".showStartEnd").slideDown(500);
-                startPlace=e.name;
-                console.log(startPlace)
-                $(".startPlace").html(startPlace)
-            };
-            // 起点取消按钮事件
-            cancelStart = function () {
-                $(".setStart").hide(500);
+
+                // 定义起点位置
+                startPlace = e.name;
+                $(".startPlace").html(startPlace);
+                p++;
             };
             click++;
         } else {
-            $(".setEnd").show(500);
-
-            // 设置终点函数
-            setEnd = function () {
-                // 设置终点nodeId
-                map.endPID = e.nodeId;
-                endPID = e.nodeId;
-                $(".setEnd").hide(500);
-                if (startPID & endPID) {
-                    if (startPID == endPID) {
-                        alert("起点终点相同，请重新设置");
-                    }
-                    endPlace=e.name;
-                    console.log(endPlace)
-                    $(".endPlace").html(endPlace)
-                    map.searchPath(function (msg) {
-                        if (msg.type == 'done') {
-                            // 地图缩放比
-                            map.zoomLevel(12);
-                            // 设置地图仰角
-                            map.setPolarAngle(40);
-                            //  显示取消寻路按钮
-                            $(".cancelSearch").show(500);
-                            // 取消寻路按钮事件
-                            cancelSearch = function () {
-                                map.clearPath(true);
-                                map.zoomLevel(20);
-                                map.setPolarAngle(0);
-                                $(".cancelSearch").hide(500);
-                                // 起点终点显示模块隐藏
-                                $(".showStartEnd").slideUp(500);
-                            }
-                        } else {
-                            alert('寻路失败');
+            if (p == 2) {
+                $(".setEnd").show(500);
+                // 设置终点函数
+                setEnd = function() {
+                    // 设置终点nodeId
+                    endPID = e.nodeId;
+                    map.endPID = e.nodeId;
+                    $(".setEnd").hide(500);
+                    // 判断起点终点是否一致，一致提醒错误
+                    if (startPID & endPID) {
+                        if (startPID == endPID) {
+                            alert("起点终点相同，请重新设置");
                         }
-                    })
-                }
-            };
+                        // 定义终点位置
+                        endPlace = e.name;
+                        $(".endPlace").html(endPlace);
 
-            // 起点取消按钮事件
-            cancelEnd = function () {
-                $(".setEnd").hide(500);
-            };
+                        // 寻路事件
+                        map.searchPath(function(msg) {
+                            if (msg.type == "done") {
+                                // 地图缩放比
+                                map.zoomLevel(12);
+                                // 设置地图仰角
+                                map.setPolarAngle(40);
+                                //  显示取消寻路按钮
+                                $(".cancelSearch").show(500);
+                            } else {
+                                alert("寻路失败");
+                                map.clearPath(true);
+                            }
+                        });
+                    }
+                    p--;
+                };
+            }
             click--;
         }
+        console.log(click);
     }
+    // 起点终点调转事件
+    $(".reverse").on("click", function() {
+        if (k == 1) {
+            map.endPID = startPID;
+            map.startPID = endPID;
+            $(".startPlace").html(endPlace);
+            $(".endPlace").html(startPlace);
+            k++;
+        } else {
+            map.endPID = endPID;
+            map.startPID = startPID;
+            $(".startPlace").html(startPlace);
+            $(".endPlace").html(endPlace);
+            k--;
+        }
+    });
+
+    // 寻路取消按钮
+    cancelSearch = function() {
+        map.clearPath(true);
+        map.zoomLevel(20);
+        map.setPolarAngle(0);
+        $(".cancelSearch").hide(500);
+        // 起点终点显示模块隐藏
+        $(".showStartEnd").slideUp(500);
+    };
+
+    // 起点取消按钮事件
+    cancelStart = function() {
+        $(".setStart").hide(500);
+        cancelSearch();
+        click--;
+    };
+
+    // 终点取消按钮事件
+    cancelEnd = function() {
+        $(".setEnd").hide(500);
+        click++;
+    };
+
+    //  起点终点显示模块back事件
+    $(".back").on("click", function() {
+        cancelSearch();
+        // 清除起点终点内容
+        startPID="";
+        endPID="";
+        $(".startPlace").html("");
+        $(".endPlace").html("");
+        click=1;
+        p=1;
+    });
 });
